5cb2a3251fd5c62e7d7e23fb0b7743140d90bafa
Egor Solonin <e.solonin@onpoint.ru>
2025-02-21 22:03:32 +0300

    Updates

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 100, // need reload eslint
+
+  semi: false,
+  useTabs: false,
+  singleQuote: true,
+  bracketSpacing: true,
+
+  jsxSingleQuote: true,
+
+  tabWidth: 2,
+  endOfLine: 'auto',
+  trailingComma: 'all',
+};
